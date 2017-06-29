5790b02ccb7e833417accab62e8e72c520a9374f
Alexander Krupenkin <mail@akru.me>
2017-06-29 18:17:04 +0300

    Simplified turnout commands

--- src/System/Hardware/Z21/Actions.hs
+++ src/System/Hardware/Z21/Actions.hs
@@ -20,7 +20,7 @@ data Action
   | XGetLocoInfo Address
   | XSetLocoDrive Address Word8 Word8
   | XGetTurnoutInfo Address
-  | XSetTurnout Address Bool Bool Bool
+  | XSetTurnout Address Word8
 
 pack :: Action -> Packet
 pack GetSerial              = Packet 0x10 ()
@@ -28,10 +28,7 @@ pack Logoff                 = Packet 0x30 ()
 pack (XGetLocoInfo a)       = Packet 0x40 $ XBus 0xE3 $ LBS.pack [0xF0, msb a, lsb a]
 pack (XSetLocoDrive a s rv) = Packet 0x40 $ XBus 0xE4 $ LBS.pack [s, msb a, lsb a, rv]
 pack (XGetTurnoutInfo a)    = Packet 0x40 $ XBus 0x43 $ LBS.pack [msb a, lsb a]
-pack (XSetTurnout ad q a p) = Packet 0x40 $ XBus 0x53 $ LBS.pack [msb ad, lsb ad, cmd]
-    where cmd = bit 8 .|. if q then bit 6 else zeroBits
-                      .|. if a then bit 4 else zeroBits
-                      .|. if p then bit 1 else zeroBits
+pack (XSetTurnout a cmd)    = Packet 0x40 $ XBus 0x53 $ LBS.pack [msb a, lsb a, cmd]
 
 unpack :: Binary a => Packet -> Maybe a
 unpack (Packet _ dat) =
@@ -63,5 +60,5 @@ getTurnoutInfo a = do
     res <- await
     return (B16.encode . xbusData <$> (res >>= unpack))
 
-setTurnout:: Address -> Bool -> Bool -> Bool -> Z21 ()
-setTurnout a b c d = yield $ pack (XSetTurnout a b c d)
+setTurnout:: Address -> Word8 -> Z21 ()
+setTurnout a c = yield $ pack (XSetTurnout a c)
